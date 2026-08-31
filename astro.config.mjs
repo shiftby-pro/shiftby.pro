a5e19defac14d9a76444a4d40693e53b4123dfcf
manandakrishna <manandakrishna@gmail.com>
2026-09-01 03:44:01 +0800

    build: bootstrap Astro publication site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  site: 'https://shiftby.pro',
+  trailingSlash: 'always',
+  integrations: [mdx()],
+});
