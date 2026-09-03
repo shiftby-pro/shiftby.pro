d1f96eefebb9d7095d53fbcf138d145307ced266
manandakrishna <manandakrishna@gmail.com>
2026-09-03 10:35:28 +0800

    Use Vercel deployment as temporary canonical origin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://shiftby.pro',
+  site: 'https://shiftby-pro.vercel.app',
   trailingSlash: 'always',
   integrations: [mdx()],
 });
