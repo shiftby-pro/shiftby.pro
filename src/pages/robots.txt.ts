336214ddde25673cff9af519722e67f6ca275a51
manandakrishna <manandakrishna@gmail.com>
2026-09-03 10:35:35 +0800

    Point robots sitemap at temporary canonical host

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,5 +1,5 @@
 export function GET() {
-  return new Response('User-agent: *\nAllow: /\nSitemap: https://shiftby.pro/sitemap.xml\n', {
+  return new Response('User-agent: *\nAllow: /\nSitemap: https://shiftby-pro.vercel.app/sitemap.xml\n', {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' },
   });
 }
