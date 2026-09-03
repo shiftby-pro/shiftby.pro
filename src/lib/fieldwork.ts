7daca6c2c1c9781b18bc1256c8332d3f22c69b43
manandakrishna <manandakrishna@gmail.com>
2026-09-03 19:20:38 +0800

    Expose shared fieldwork publication predicates

--- src/lib/fieldwork.ts
+++ src/lib/fieldwork.ts
@@ -1,9 +1,9 @@
 import type { CollectionEntry } from 'astro:content';
-import { isPublishedIndexable, isReadyToPublish } from './content-taxonomy';
+import { isPublishedIndexable, isReadyToPublish, isReviewNoindex } from './content-taxonomy';
 
 export type FieldworkEntry = CollectionEntry<'fieldwork'>;
 
-export { isPublishedIndexable, isReadyToPublish };
+export { isPublishedIndexable, isReadyToPublish, isReviewNoindex };
 
 export function getFieldworkCanonicalPath(item: Pick<FieldworkEntry, 'data'>): string {
   return item.data.canonical_url;
